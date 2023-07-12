aa1e539339c75289522ccd3040f100266512f899
Gustavo Cortez <gustavocortez@lm-147dda1d9e78.dhcp.usask.ca>
2023-07-12 12:03:55 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>SITE DOGO GUTABO NAFRAVIA</h1>
+        <p>SITE DA NAFRAVIA SAFADA</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
